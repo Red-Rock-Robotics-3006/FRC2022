7248714d04cbf631842aa128b11e86c92f909f73
Anastasia Dunca (Student) <anastasia.d960@slcstudents.org>
2022-02-24 16:48:08 -0700

    shooter test code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,10 +5,14 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -22,6 +26,10 @@ public class RobotContainer {
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
+  private final Joystick mechJoystick = new Joystick(0);
+  private final ShooterSubsystem shooter = new ShooterSubsystem();
+  
+
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
@@ -34,7 +42,9 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    new JoystickButton(mechJoystick, 3).whenPressed(new InstantCommand(() -> shooter.shoot())).whenReleased(new InstantCommand(() -> shooter.stop()));
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+  
+  private WPI_TalonFX shootMotor = new WPI_TalonFX(1);
+  
+
+  public ShooterSubsystem() {
+    this.shootMotor.configFactoryDefault();
+    this.shootMotor.setInverted(true);
+    this.shootMotor.setNeutralMode(NeutralMode.Brake);
+
+  }
+
+  @Override
+  public void periodic() {
+    
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    
+  }
+
+  public void shoot()
+  {
+    this.shootMotor.setVoltage(1);
+  }
+
+  public void stop()
+  {
+    this.shootMotor.setVoltage(0);
+  }
+}
